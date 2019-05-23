04d75202f823ffb16f6babb267cbdeb478c5d1ad
Trung Hai Nguyen <nguyentrungh@gmail.com>
2019-05-23 14:48:00 +0700

    write docstring for receptor_ligand_separation()

--- scripts/_amber.py
+++ scripts/_amber.py
@@ -1,25 +1,31 @@
 
 import sys
-sys.path.append("/home/tnguye46/opt/src/Bpmf_with_FFT")
+sys.path.append("/home/tnguye46/opt/src/BPMFwFFT/bpmfwfft")
 from IO import PrmtopLoad
+
 
 def receptor_ligand_separation(complex_prmtop_file, receptor_prmtop_file, ligand_prmtop_file):
     """
-    assuming the ligand is either at top or bottom of the complex
+    :param complex_prmtop_file: str
+    :param receptor_prmtop_file: str
+    :param ligand_prmtop_file: str
+    :return: (receptor_indices, ligand_indices)
+            receptor_indices: list of int, list of receptor atom index
+            ligand_indices: list of int, list of ligand atom index
     """
-    complex_prmtop  = PrmtopLoad(complex_prmtop_file).get_all_parameters()
+    complex_prmtop = PrmtopLoad(complex_prmtop_file).get_all_parameters()
     receptor_prmtop = PrmtopLoad(receptor_prmtop_file).get_all_parameters()
-    ligand_prmtop   = PrmtopLoad(ligand_prmtop_file).get_all_parameters()
+    ligand_prmtop = PrmtopLoad(ligand_prmtop_file).get_all_parameters()
 
-    assert len( ligand_prmtop["RESIDUE_LABEL"] ) == 1, "There are none or more than one ligand molecules"
-    assert len( ligand_prmtop["RESIDUE_LABEL"] ) + len( receptor_prmtop["RESIDUE_LABEL"] ) == len( complex_prmtop["RESIDUE_LABEL"] ), \
+    assert len(ligand_prmtop["RESIDUE_LABEL"]) == 1, "There are none or more than one ligand molecules"
+    assert len(ligand_prmtop["RESIDUE_LABEL"]) + len(receptor_prmtop["RESIDUE_LABEL"]) == len( complex_prmtop["RESIDUE_LABEL"] ), \
             "Number of residues of receptor plus ligand is not the same as complex"
 
     ligand_resname = ligand_prmtop["RESIDUE_LABEL"][0]
 
     n_complex_atoms = complex_prmtop["POINTERS"]["NATOM"]
     n_recptor_atoms = receptor_prmtop["POINTERS"]["NATOM"]
-    n_ligand_atoms  = ligand_prmtop["POINTERS"]["NATOM"]
+    n_ligand_atoms = ligand_prmtop["POINTERS"]["NATOM"]
 
     if complex_prmtop["RESIDUE_LABEL"][0] == ligand_resname:
         ligand_indices = range(0, n_ligand_atoms)
@@ -27,7 +33,7 @@ def receptor_ligand_separation(complex_prmtop_file, receptor_prmtop_file, ligand
         
     elif complex_prmtop["RESIDUE_LABEL"][-1] == ligand_resname:
         receptor_indices = range(0, n_recptor_atoms)
-        ligand_indices   = range(n_recptor_atoms, n_complex_atoms)
+        ligand_indices = range(n_recptor_atoms, n_complex_atoms)
 
     else:
         raise Exception("Ligand is not either on top or bottom of complex")
