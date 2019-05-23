c11628bd325714cb42ceb60e48459655aedbca8e
Trung Hai Nguyen <nguyentrungh@gmail.com>
2019-05-23 15:02:21 +0700

    clean up and write docstring

--- scripts/_mbar.py
+++ scripts/_mbar.py
@@ -2,10 +2,12 @@
 import numpy as np
 import pymbar
 
+
 def run_mbar(u_kln, N_k):
     """
-    u_kln   :   ndarray, reduced energy matrix
-    N_k     :   ndarray, number of samples at state k
+    :param u_kln: 3d numpy array,  reduced potential energy
+    :param N_k: 1d numpy array, number of samples at state k
+    :return: mbar, an object of pymbar.MBAR
     """
     K = len(N_k)
     f_k_BAR = np.zeros(K)
@@ -18,11 +20,13 @@ def run_mbar(u_kln, N_k):
     mbar = pymbar.MBAR(u_kln, N_k, verbose = True, initial_f_k = f_k_BAR)
     return mbar
 
+
 def mbar_weights_in_state(u_kln, N_k, state_index):
     """
-    u_kln   :   ndarray, reduced energy matrix
-    N_k     :   ndarray, number of samples at state k
-    state_index   :   int, usually for YANK 0 is fully interacting, K-1 is fully noninteracting
+    :param u_kln: 3d numpy array,  reduced potential energy
+    :param N_k: 1d numpy array, number of samples at state k
+    :param state_index: int, usually for YANK 0 is fully interacting, K-1 is fully noninteracting
+    :return: weights, numpy array
     """
     if state_index >= 0:
         assert 0 <= state_index < u_kln.shape[0], "state index out of range"
@@ -33,5 +37,11 @@ def mbar_weights_in_state(u_kln, N_k, state_index):
     weights = weights[:, state_index]
     return weights
 
+
 def mbar_weights_in_holo(u_kln, N_k):
+    """
+    :param u_kln: 3d numpy array,  reduced potential energy
+    :param N_k: 1d numpy array, number of samples at state k
+    :return: weights, numpy array
+    """
     return mbar_weights_in_state(u_kln, N_k, 0)
