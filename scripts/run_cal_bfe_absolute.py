b354cd965c0a8d6b5ab770e85c49936f70f8ea94
Trung Hai Nguyen <nguyentrungh@gmail.com>
2019-05-27 09:26:21 +0700

    done cleaning up

--- scripts/run_cal_bfe_absolute.py
+++ scripts/run_cal_bfe_absolute.py
@@ -7,11 +7,11 @@ import os
 import argparse
 import glob
 import numpy as np
-import copy
+
 np.seterr(over='raise')     # raise exception if overload
 
 from _absolute_estimators import MultiStruScores
-from _algdock import load_algdock_snapshots_for_each_of_six_yank_systems
+from _weight_processing import equalize_system_weights, take_6_apo, take_12_near_apo, take_24_near_apo
 
 
 def averaging(scores_dir, ligand_3l_codes, yank_systems, result_dir, weights, combining_rules):
@@ -66,67 +66,6 @@ def averaging(scores_dir, ligand_3l_codes, yank_systems, result_dir, weights, co
             out_files[rule][FF].close()
 
     return None
-
-
-def equalize_system_weights(original_weights):
-    """
-    make all the ref systems equally weighted
-    :param original_weights: dict
-    :return: new_weights, dict
-    """
-    new_weights = copy.deepcopy(original_weights)
-    for system in new_weights["systems"].keys():
-        new_weights["systems"][system] = 1.0
-    return new_weights
-
-
-def take_6_apo(original_weights):
-    """
-    only snapshots from apo of YANK states get nonzero weights
-    :param original_weights: dict
-    :return: new_weights
-    """
-    new_weights = equalize_system_weights(original_weights)
-    ordered_snapshots = load_algdock_snapshots_for_each_of_six_yank_systems() 
-
-    for system in new_weights["systems"].keys():
-        for i, snapshot in enumerate(ordered_snapshots[system]):
-            if i < len(ordered_snapshots[system]) - 6:
-                new_weights[system][snapshot] = 0.
-            else:
-                new_weights[system][snapshot] = 1.
-            
-    return new_weights
-
-
-def take_12_near_apo(original_weights):
-    """
-    :param original_weights: dict
-    :return: dict
-    """
-    new_weights = equalize_system_weights(original_weights)
-    ordered_snapshots = load_algdock_snapshots_for_each_of_six_yank_systems()
-
-    for system in new_weights["systems"].keys():
-        for i, snapshot in enumerate(ordered_snapshots[system]):
-            if i < len(ordered_snapshots[system]) - 12:
-                new_weights[system][snapshot] = 0.
-    return new_weights
-
-
-def take_24_near_apo(original_weights):
-    """
-    :param original_weights: dict
-    :return: dict
-    """
-    new_weights = equalize_system_weights(original_weights)
-    ordered_snapshots = load_algdock_snapshots_for_each_of_six_yank_systems()
-
-    for system in new_weights["systems"].keys():
-        for i, snapshot in enumerate(ordered_snapshots[system]):
-            if i < len(ordered_snapshots[system]) - 24:
-                new_weights[system][snapshot] = 0.
-    return new_weights
 
 
 DOCK6_SUB_DIR = "dock6"
