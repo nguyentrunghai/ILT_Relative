b662db53f961051445042e738327a08504420eb4
Trung Hai Nguyen <nguyentrungh@gmail.com>
2019-05-27 09:41:42 +0700

    add functions for holo

--- scripts/_weight_processing.py
+++ scripts/_weight_processing.py
@@ -66,3 +66,52 @@ def take_24_near_apo(original_weights):
     return new_weights
 
 
+def take_6_holo(original_weights):
+    """
+    only snapshots from apo of YANK states get nonzero weights
+    :param original_weights: dict
+    :return: new_weights
+    """
+    new_weights = equalize_system_weights(original_weights)
+    ordered_snapshots = load_algdock_snapshots_for_each_of_six_yank_systems()
+
+    for system in new_weights["systems"].keys():
+        for i, snapshot in enumerate(ordered_snapshots[system]):
+            if i < 6:
+                new_weights[system][snapshot] = 1.
+            else:
+                new_weights[system][snapshot] = 0.
+
+    return new_weights
+
+
+def take_12_near_holo(original_weights):
+    """
+    :param original_weights: dict
+    :return: dict
+    """
+    new_weights = equalize_system_weights(original_weights)
+    ordered_snapshots = load_algdock_snapshots_for_each_of_six_yank_systems()
+
+    for system in new_weights["systems"].keys():
+        for i, snapshot in enumerate(ordered_snapshots[system]):
+            if i >= 12:
+                new_weights[system][snapshot] = 0.
+
+    return new_weights
+
+
+def take_24_near_holo(original_weights):
+    """
+    :param original_weights: dict
+    :return: dict
+    """
+    new_weights = equalize_system_weights(original_weights)
+    ordered_snapshots = load_algdock_snapshots_for_each_of_six_yank_systems()
+
+    for system in new_weights["systems"].keys():
+        for i, snapshot in enumerate(ordered_snapshots[system]):
+            if i >= 24:
+                new_weights[system][snapshot] = 0.
+
+    return new_weights
