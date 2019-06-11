dc0df5f0139fc13ea84aa8f1b4ac229df4a59f37
Trung Hai Nguyen <nguyentrungh@gmail.com>
2019-06-11 09:01:10 +0700

    move the function relative_bfe_with_cv

--- scripts/_relative_estimators.py
+++ scripts/_relative_estimators.py
@@ -435,3 +435,80 @@ def bootstrap_estimate_cov_var(score_dir, target_ligand, ref_ligand, all_ref_lig
 
     return rel_fes, self_rel_fes, covariance, variance
 
+
+def relative_bfe_with_cv(snapshots, score_dir, target_ligand, ref_ligand,
+            weights, yank_interaction_energies,
+            FF, bootstrap_repeats):
+    """
+    calculate Cov(relative_bfe, self_relative_bfe) and Var(self_relative_bfe)
+    using bootstrap
+
+    :param snapshots: list of str
+    :param score_dir: str
+    :param target_ligand: str
+    :param ref_ligand: str
+    :param weights: dict,
+                    weights[ref_ligand_name][snapshot] -> float
+                    weights["systems"][ref_ligand_name] -> float
+    :param yank_interaction_energies: dict, yank_interaction_energies[system][snapshot] -> float
+    :param FF: str, phase
+    :param bootstrap_repeats: int
+    :return: (result, error, rel_fes, self_rel_fes)
+            result, float, relative binding free energy
+            error, float, standard error
+            rel_fes, numpy array of shape (bootstrap_repeats,),
+                        "naive" estimate of relative binding free energies for different bootstrap samples
+            self_rel_fes, numpy array of shape (bootstrap_repeats,)
+                         estimate of self relative binding free energies for different bootstrap samples
+    """
+    all_ref_ligands = [ligand for ligand in weights.keys() if ligand != "systems"]
+    assert ref_ligand in all_ref_ligands, "Unknown ref ligand: " + ref_ligand
+    assert set(snapshots) <= set(weights[ref_ligand].keys()), "snapshots must be a subset of weights[ref_ligand].keys()"
+
+    n_snapshots = len(snapshots)
+
+    target_ligand_group = target_ligand[:-3]
+    target_ligand_3l_code = target_ligand[-3:]
+
+    ref_ligand_group = ref_ligand[: -3]
+    ref_ligand_3l_code = ref_ligand[-3:]
+
+    rel_fes = []
+    self_rel_fes = []
+    for _ in range(bootstrap_repeats):
+        random_snapshots = np.random.choice(snapshots, size=n_snapshots, replace=True)
+
+        rel_fe = RelBFEWithoutCV(score_dir, target_ligand_group, target_ligand_3l_code,
+                                 weights, [ref_ligand], yank_interaction_energies
+                                 ).cal_exp_mean_separate_for_each_system(FF, random_snapshots)
+        rel_fe = rel_fe.values()[0]
+
+        self_rel_fe = RelBFEWithoutCV(score_dir, ref_ligand_group, ref_ligand_3l_code,
+                                      weights, [ref_ligand], yank_interaction_energies
+                                      ).cal_exp_mean_separate_for_each_system(FF, random_snapshots)
+        self_rel_fe = self_rel_fe.values()[0]
+
+        if (rel_fe not in [np.nan, np.inf, -np.inf]) and (self_rel_fe not in [np.nan, np.inf, -np.inf]):
+            rel_fes.append(rel_fe)
+            self_rel_fes.append(self_rel_fe)
+
+    rel_fes = np.array(rel_fes)
+    self_rel_fes = np.array(self_rel_fes)
+
+    covariance = np.cov(rel_fes, self_rel_fes)[0, -1]
+    variance = np.var(self_rel_fes)
+
+    rel_fe = RelBFEWithoutCV(score_dir, target_ligand_group, target_ligand_3l_code,
+                             weights, [ref_ligand], yank_interaction_energies
+                             ).cal_exp_mean_separate_for_each_system(FF, snapshots)
+    rel_fe = rel_fe.values()[0]
+
+    self_rel_fe = RelBFEWithoutCV(score_dir, ref_ligand_group, ref_ligand_3l_code,
+                                  weights, [ref_ligand], yank_interaction_energies
+                                  ).cal_exp_mean_separate_for_each_system(FF, snapshots)
+    self_rel_fe = self_rel_fe.values()[0]
+
+    result = rel_fe - (covariance / variance) * self_rel_fe
+    error = (rel_fes - (covariance / variance) * self_rel_fes).std()
+
+    return result, error, rel_fes, self_rel_fes
