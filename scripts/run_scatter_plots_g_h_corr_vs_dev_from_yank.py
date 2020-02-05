2161170f084efb7f2a9007e9a6e94c94171f6327
Trung Haii Nguyen <nguyentrungh@gmail.com>
2020-02-05 15:49:14 +0700

    load g-h correlation

--- scripts/run_scatter_plots_g_h_corr_vs_dev_from_yank.py
+++ scripts/run_scatter_plots_g_h_corr_vs_dev_from_yank.py
@@ -38,17 +38,18 @@ target_ligands = [ligand for ligand in all_ligands if ligand not in ref_ligands]
 
 # deviations from yank of bfe est WITH CV
 devs_with_cv = {}            # devs_with_cv[ref_ligand][target_ligand] -> float
-
+bfe_errors_with_cv = {}
 for ref_ligand in ref_ligands:
     infile = os.path.join(args.bfe_with_cv_dir, ref_ligand + args.result_dir_suffix,
                           args.combining_rule, args.rel_bfe_file)
     print("Loading", infile)
-    bfes, _ = load_scores(infile, 0, 1, 2, exclude_ligands=ref_ligands)
+    bfes, errors = load_scores(infile, 0, 1, 2, exclude_ligands=ref_ligands)
 
     for target_ligand in bfes:
         bfes[target_ligand] = bfes[target_ligand] + yank_bfes[ref_ligand] - yank_bfes[target_ligand]
 
     devs_with_cv[ref_ligand] = bfes
+    bfe_errors_with_cv[ref_ligand] = errors
 
 
 # deviations from yank of bfe est WITHOUT CV
@@ -68,28 +69,19 @@ for ref_ligand in ref_ligands:
 
     devs_without_cv[ref_ligand] = bfes
 
-# load C and g-h correlation coefficients
-cs = {}
-c_errors = {}
-corr_coeffs = {}
-corr_errors = {}
+# load g-h correlation coefficients
+corr_coeffs = {}      # corr_coeffs[ref_ligand][target_ligand] -> float
 
 for ref_ligand in ref_ligands:
-    cs[ref_ligand] = {}
-    c_errors[ref_ligand] = {}
     corr_coeffs[ref_ligand] = {}
-    corr_errors[ref_ligand] = {}
 
     for target_ligand in target_ligands:
-        infile = os.path.join(args.rel_bfe_dir, ref_ligand + args.result_dir_suffix,
+        infile = os.path.join(args.g_h_corr_dir, ref_ligand + args.result_dir_suffix,
                               args.combining_rule, ref_ligand + "_G_CORR_H_" + target_ligand)
         print("Loading", infile)
         c, c_err, corr, corr_err = np.loadtxt(infile)
 
-        cs[ref_ligand][target_ligand] = c
-        c_errors[ref_ligand][target_ligand] = c_err
         corr_coeffs[ref_ligand][target_ligand] = corr
-        corr_errors[ref_ligand][target_ligand] = corr_err
 
 
 FONTSIZE = 8
