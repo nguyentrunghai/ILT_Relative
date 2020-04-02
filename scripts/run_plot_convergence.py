0351b267ff7c391c201d797c362c275fcdbf9a44
Trung Haii Nguyen <nguyentrungh@gmail.com>
2020-04-02 14:47:42 +0700

    add funcs scaling, plot_convergence

--- scripts/run_plot_convergence.py
+++ scripts/run_plot_convergence.py
@@ -18,12 +18,15 @@ parser.add_argument("--data_1_dir", type=str, default="Relative_FE_Est_1")
 parser.add_argument("--data_2_dir", type=str, default="Relative_FE_Est_with_CV_method_3a")
 parser.add_argument("--data_file", type=str, default="r_rmse.dat")
 
-# "pearson_R" or "RMSE"
+parser.add_argument("--ref_systems",
+type=str, default="1-methylpyrrole.A__AAA benzene.A__AAA lysozyme.active.A__ABJ lysozyme.inactive.A__AAS phenol.A__AAA p-xylene.A__AAA")
+
+# "Pearson_R" or "RMSE"
 parser.add_argument("--which_data", type=str, default="none")
 
-# to modify data_2
-parser.add_argument("--ref_ligands_2_modify", type=str, default=" ")
-parser.add_argument("--modifying_constants", type=str, default=" ")
+parser.add_argument("--y2_scale_facs", type=str, default="")
+parser.add_argument("--y2_shifts", type=str, default="")
+parser.add_argument("--y2err_scale_facs", type=str, default="")
 
 parser.add_argument("--xlabel", type=str, default="# receptor snapshots")
 parser.add_argument("--ylabel", type=str, default="Pearson's R w.r.t. final results")
@@ -37,7 +40,7 @@ args = parser.parse_args()
 
 def _load_data(data_file, which_data):
     data = np.loadtxt(data_file)
-    if which_data == "pearson_R":
+    if which_data == "Pearson_R":
         size, r, r_std = data[:, 0],  data[:, 1], data[:, 2]
         return size, r, r_std
     elif which_data == "RMSE":
@@ -47,20 +50,47 @@ def _load_data(data_file, which_data):
         raise ValueError("Unknown which_data: " + which_data)
 
 
-ref_ligands_2_modify = args.ref_ligands_2_modify.split()
-modifying_constants = [np.float(s) for s in args.modifying_constants.split()]
-assert len(ref_ligands_2_modify) == len(modifying_constants), "len(ref_ligands_2_modify) neq len(modifying_constants)"
+def scaling(x, fac=1.):
+    x = x[::-1]
+    facs = np.linspace(1, fac, len(x))
+    x = x*facs
+    return x[::-1]
+
+
+def plot_convergence(data_file_1, data_file_2, which_data, ax,
+                     y2_scale=1, y2err_scale=1, y2_shift=0,
+                     colors=("b", "r"), line_styles=["-", "--"],
+                     lw=1):
+    x1, y1, yerr1 = _load_data(data_file_1, which_data)
+    x2, y2, yerr2 = _load_data(data_file_2, which_data)
+
+    yerr1 = yerr1 / 2
+    yerr2 = yerr2 / 2
+    yerr2 = yerr2 * y2err_scale
+
+    y2 = scaling(y2, fac=y2_scale) + y2_shift
+
+    ax.errorbar(x1, y1, yerr=yerr1, c=colors[0], linestyle=line_styles[0], lw=lw)
+    ax.errorbar(x2, y2, yerr=yerr2, c=colors[1], linestyle=line_styles[1], lw=lw)
+
+    return None
+
 
 colors = args.colors.split()
 line_styles = args.line_styles.split()
 assert len(colors) == len(line_styles) == 2, "len(colors) and len(line_styles) must equal 2"
 
-modifying_constants = {ligand : num for ligand, num in zip(ref_ligands_2_modify, modifying_constants)}
-print("modifying_constants", modifying_constants)
+ref_systems = args.ref_systems.split()
+print("ref_systems:", ref_systems)
 
-_, _, single_snap_weights, _, _ = load_mbar_weights()
+y2_scale_facs = args.y2_scale_facs.split()
+if len(y2_scale_facs) == 0:
+    y2_scale_facs = [1 for _ in ref_systems]
+else:
+    assert len(y2_scale_facs) == len(ref_systems), "wrong len of y2_scale_facs"
+    y2_scale_facs = [float(s) for s in y2_scale_facs]
+print("y2_scale_facs")
 
-ref_ligands = [ligand for ligand in single_snap_weights.keys() if ligand != "systems"]
 for ref_ligand in ref_ligands:
     data_file_1 = os.path.join(args.data_1_dir, ref_ligand, args.data_file)
     print("Loading " + args.which_data + " from " + data_file_1)
