ce59ca876e79b1c5dc4d06c2fc6dad91eb0252dd
Trung Haii Nguyen <nguyentrungh@gmail.com>
2020-04-10 14:32:38 +0700

    add 4b

--- scripts/run_cal_bfe_relative_with_cv_using_exp_mean.py
+++ scripts/run_cal_bfe_relative_with_cv_using_exp_mean.py
@@ -17,6 +17,7 @@ from _relative_estimators import relative_bfe_with_cv_using_exp_mean_method_2b
 from _relative_estimators import relative_bfe_with_cv_using_exp_mean_method_3a
 from _relative_estimators import relative_bfe_with_cv_using_exp_mean_method_3b
 from _relative_estimators import relative_bfe_with_cv_using_exp_mean_method_4a
+from _relative_estimators import relative_bfe_with_cv_using_exp_mean_method_4b
 
 parser = argparse.ArgumentParser()
 
@@ -34,7 +35,7 @@ parser.add_argument("--result_dir_suffix", type=str, default="__equal_sys__singl
 
 parser.add_argument("--combining_rule", type=str, default="ExpMean")
 
-# method is either 2a, 2b, 3a, 3b, 4a
+# method is either 2a, 2b, 3a, 3b, 4a, 4b
 parser.add_argument("--method", type=str, default="None")
 
 parser.add_argument("--remove_outliers_g_h", action="store_true", default=False)
@@ -88,6 +89,10 @@ elif args.method == "3b":
 elif args.method == "4a":
     print("Method 4a")
     relative_bfe_with_cv_using_exp_mean = relative_bfe_with_cv_using_exp_mean_method_4a
+
+elif args.method == "4b":
+    print("Method 4b")
+    relative_bfe_with_cv_using_exp_mean = relative_bfe_with_cv_using_exp_mean_method_4b
 
 else:
     raise ValueError("unrecognized method " + args.method)
